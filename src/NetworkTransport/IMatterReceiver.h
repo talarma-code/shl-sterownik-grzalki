6036e0f2ce1f99ee78b6bb067c8c7e5091bf5019
Marek Talarczyk <talarma@gmail.com>
2025-11-22 19:17:40 +0100

    added submodule for matter like protocol v2

--- src/NetworkTransport/IMatterReceiver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "MatterLikePacket.h"
-
-class IMatterReceiver {
-public:
-    virtual void handlePacket(const MatterLikePacket &pkt, const uint8_t *srcMac) = 0;
-    virtual ~IMatterReceiver() = default;
-};
